b4695254265128a46104375023893a3f44b8eb21
renatoimportador <renato.gomesd@icloud.com>
2026-06-16 19:41:55 -0300

    Update ListarTreinos.tsx

--- src/pages/admin/ListarTreinos.tsx
+++ src/pages/admin/ListarTreinos.tsx
@@ -238,7 +238,7 @@ export function ListarTreinos() {
     try {
   await excluirTreino(id)
 
-  setTreinos(prev => prev.filter(treino => treino.id !== id))
+  setTreinos((prev: any[] => prev.filter((treino) => treino.id !== id))
 
   toast.success('Treino excluído!')
 } catch {
