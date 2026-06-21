8a3647b3b26130866cc303328832127ca76e372a
renatoimportador <renato.gomesd@icloud.com>
2026-06-21 11:53:34 -0300

    Update routes.tsx

--- olympea-community/src/app/routes.tsx
+++ olympea-community/src/app/routes.tsx
@@ -68,7 +68,7 @@ export function AppRoutes() {
 
       <Route
         element={
-          <ProtectedRoute roles={['admin']}>
+          <ProtectedRoute roles={['head_coach']}>
             <AdminLayout />
           </ProtectedRoute>
         }
